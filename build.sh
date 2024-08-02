e17bfa00a4c1a2da900ca4955ded877267b613e4
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-08-02 11:45:07 -0500

    same error

--- build.sh
+++ build.sh
@@ -13,3 +13,6 @@ python manage.py migrate
 
 # Construir Tailwind CSS
 npx tailwindcss -i TaskStat/staticfiles/css/dist/styles.css -o TaskStat/staticfiles/css/dist/output.css --minify
+
+# Explicitly install gunicorn (just in case)
+pip install gunicorn
