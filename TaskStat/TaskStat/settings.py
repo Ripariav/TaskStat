eae4bd81beda4496bf48bdbc0c162f8bd3fb4ece
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-08-01 23:48:39 -0500

    last settings

--- TaskStat/TaskStat/settings.py
+++ TaskStat/TaskStat/settings.py
@@ -103,7 +103,6 @@ WSGI_APPLICATION = 'TaskStat.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
