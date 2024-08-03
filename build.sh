c7675166a52c57d0434a2ab345fa4a05fe17eab0
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-08-03 11:58:43 -0500

    still under check

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Instalar dependencias
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Convertir archivos estáticos
 python manage.py collectstatic --no-input
